5d60e95e2a61620e3ac23e5157e68f0608b9c78b
Kilian McMahon <kil@kilmc.com>
2022-09-22 20:57:42 +0200

    feat: add parser functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,70 @@
 <script lang="ts">
+	let song = `Verse 1 ---
+E           A@@@
+Her name is Noelle`;
+
+	const sectionIndicator = '---';
+	const chordIndicator = '@@@';
+
+	type TLineType = 'section' | 'chord' | 'lyric';
+
+	const determineLineType = (line: string): TLineType => {
+		const isSectionLine = new RegExp(`${sectionIndicator}$`).test(line);
+		const isChordLine = new RegExp(`${chordIndicator}$`).test(line);
+
+		if (isSectionLine) {
+			return 'section';
+		}
+
+		if (isChordLine) {
+			return 'chord';
+		}
+
+		return 'lyric';
+	};
+
+	const parse = (song: string) => {
+		return song
+			.split('\n')
+			.map((line) => {
+				switch (determineLineType(line)) {
+					case 'section':
+						return `<div class="font-bold">${line.replace(sectionIndicator, '')}</div>`;
+					case 'chord':
+						return `<div>${line.replace(chordIndicator, '')}</div>`;
+					case 'lyric':
+						return `<div>${line}</div>`;
+					default:
+						break;
+				}
+			})
+			.join('');
+	};
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Songbook</title>
 </svelte:head>
 
-<section class="bg-black">Home section</section>
+<div class="sketch-container font-mono">
+	<div class="input">
+		<textarea bind:value={song} />
+	</div>
+
+	<div class="output whitespace-pre">
+		{@html parse(song)}
+	</div>
+</div>
+
+<style>
+	.sketch-container {
+		display: flex;
+		flex-direction: column;
+		margin: 4rem auto;
+	}
+
+	.sketch {
+		font-family: monospace;
+		font-size: 1.6em;
+	}
+</style>
